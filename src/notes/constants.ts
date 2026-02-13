5483f56a228a12557835775b4bb95fdd6ccd146c
developerriyansh-flow2125 <developerriyansh@gmail.com>
2026-02-13 22:01:19 +0530

    add test for api

--- src/notes/constants.ts
+++ src/notes/constants.ts
@@ -1,12 +1,20 @@
-import { NullOrUndefined } from "../utils/types";
 import { NoteResult } from "./types";
+import path from "node:path";
 
 export class NotesConstants {
-    public static folderPath: string = "data";
-    public static filePath: string = `${this.folderPath}/notes.json`;
-    public static tempFilePath: string = `${this.folderPath}/notes.tmp`;
-    public static fileEncoding: NullOrUndefined =
-        "utf8" as unknown as NullOrUndefined;
+    static get folderPath(): string {
+        return process.env.FOLDER_PATH ?? "data";
+    }
+
+    static get filePath(): string {
+        return path.join(this.folderPath, "notes.json");
+    }
+
+    static get tempFilePath(): string {
+        return path.join(this.folderPath, "notes.json.tmp");
+    }
+
+    static fileEncoding = "utf8" as const;
 
     public static dummyNote: NoteResult[] = [
         {
